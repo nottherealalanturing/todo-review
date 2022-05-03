4299b1477741f8c680da9da58d033fcbbe931a10
Assad Isah <nottherealalanturing@gmail.com>
2022-05-03 19:44:59 +0100

    review

---
+++ src/modules/status.js
@@ -0,0 +1,11 @@
+const updateStatus = (tasks) => {
+  document.querySelectorAll('.task').forEach((val) => {
+    val.children[0].addEventListener('change', (e) => {
+      /* eslint-disable */
+      tasks.updateTaskStatus(e.target.dataset.index, e.target.checked);
+      /* eslint-enable */
+    });
+  });
+};
+
+export default updateStatus;
